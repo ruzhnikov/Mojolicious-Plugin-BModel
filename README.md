0e492031d78ee374309a466752ccb5eab65ba3a0
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2015-02-19 23:41:44 +0300

    * init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mojolicious::Plugin::Model - It's new $module
+
+# SYNOPSIS
+
+    use Mojolicious::Plugin::Model;
+
+# DESCRIPTION
+
+Mojolicious::Plugin::Model is ...
+
+# LICENSE
+
+Copyright (C) Alexander Ruzhnikov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Alexander Ruzhnikov <ruzhnikov85@gmail.com>
---
+++ lib/Mojolicious/Plugin/Model.pm
@@ -0,0 +1,39 @@
+package Mojolicious::Plugin::Model;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Plugin::Model - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojolicious::Plugin::Model;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::Model is ...
+
+=head1 LICENSE
+
+Copyright (C) Alexander Ruzhnikov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Alexander Ruzhnikov E<lt>ruzhnikov85@gmail.comE<gt>
+
+=cut
+
