bcafb87d437896f687ac21981aecb39b3b49fc54
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2017-03-29 21:47:47 +0300

    * added documentation for module Mojolicious::BModel::Base

--- lib/Mojolicious/BModel/Base.pm
+++ lib/Mojolicious/BModel/Base.pm
@@ -10,3 +10,46 @@ has config => sub {
 };
 
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::BModel::Base -- base class for models
+
+=head1 SYNOPSIS
+
+    package MyApp::Model::MyModel;
+
+    use strict;
+    use warnings;
+
+    use Mojo::Base 'Mojolicious::BModel::Base';
+
+    sub my_method {
+        my $self = shift;
+
+        say $self->app;     # your mojo application
+        say $self->config;  # your application config. It can be also called through $self->app->config
+    }
+
+    1;
+
+=head1 DESCRIPTION
+
+    This is the base class for model classes. All model classes have to be inherited from this class
+
+=head1 LICENSE
+
+Copyright (C) 2015-2017 Alexander Ruzhnikov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Alexander Ruzhnikov E<lt>ruzhnikov85@gmail.comE<gt>
+
+=cut
