995fd676056aa6691e9212310182fae9fa4ae5a9
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2017-04-04 23:05:07 +0300

    * revert changes in documentation

--- lib/Mojolicious/Plugin/BModel.pm
+++ lib/Mojolicious/Plugin/BModel.pm
@@ -177,48 +177,42 @@ Mojolicious::Plugin::BModel - Catalyst-like models in Mojolicious
 
 =head1 SYNOPSIS
 
-# Mojolicious
+    # Mojolicious
 
-```perl
-# in your app:
-sub startup {
-    my $self = shift;
+    # in your app:
+    sub startup {
+        my $self = shift;
 
-    $self->plugin( 'BModel', { %Options } );
-}
-```
+        $self->plugin( 'BModel', { %Options } );
+    }
 
-# in controller:
-```perl
-sub my_controller {
-    my $self = shift;
+    # in controller:
+    sub my_controller {
+        my $self = shift;
 
-    my $config_data = $self->model('MyModel')->get_conf_data('field');
-}
-```
+        my $config_data = $self->model('MyModel')->get_conf_data('field');
+    }
 
-# in <your_app>/lib/<namespace>/Model/MyModel.pm:
+    # in <your_app>/lib/<namespace>/Model/MyModel.pm:
 
-```perl
-use Mojo::Base 'Mojolicious::BModel::Base';
+    use Mojo::Base 'Mojolicious::BModel::Base';
 
-sub get_conf_data {
-    my ( $self, $field ) = @_;
+    sub get_conf_data {
+        my ( $self, $field ) = @_;
 
-    # as example
-    return $self->config->{field};
-}
-```
+        # as example
+        return $self->config->{field};
+    }
 
 =head1 DESCRIPTION
 
-This module provides you an ability to separate a business-logic from controllers into a 'model' class
-and use this one by the method 'model' of a controller object.
-This approach is using in the L<Catalyst framework|https://metacpan.org/pod/Catalyst>.
+    This module provides you an ability to separate a business-logic from controllers into a 'model' class
+    and use this one by the method 'model' of a controller object.
+    This approach is using in the L<Catalyst framework|https://metacpan.org/pod/Catalyst>.
 
-All model classes have to be inherited from the class 'Mojolicious::BModel::Base', examples see below.
+    All model classes have to be inherited from the class 'Mojolicious::BModel::Base', examples see below.
 
-This module works in Unix-like and Windows systems.
+    This module works in Unix-like and Windows systems.
 
 =head2 Options
 
@@ -226,16 +220,16 @@ This module works in Unix-like and Windows systems.
 
 =item B<create_dir>
 
-A boolean flag that determines automatically create the folder '<yourapp>/lib/<namespace>/Model'
-if it does not exist. 0 - do not create, 1 - create. Enabled by default
+    A boolean flag that determines automatically create the folder '<yourapp>/lib/<namespace>/Model'
+    if it does not exist. 0 - do not create, 1 - create. Enabled by default
 
 =item B<namespace>
 
-A place in the '/lib' folder of application where there are your model classes. By default it is the name of application.
-The value of this parameter should be in the format with a delimiter '::', for example 'Aaaaa::Bbbb::Cccc'.
-This string(in format 'Aaaaa::Bbbb::Cccc') will be converted to the path 'Aaaaa/Bbbb/Cccc'(or 'Aaaaa\Bbbb\Cccc'
-for Microsoft Windows) and the absolute path to the your Model dir will looks like '<your app>/lib/Aaaaa/Bbbb/Cccc/Model'
-and all Model classes will be sought in this direcory.
+    A place in the '/lib' folder of application where there are your model classes. By default it is the name of application.
+    The value of this parameter should be in the format with a delimiter '::', for example 'Aaaaa::Bbbb::Cccc'.
+    This string(in format 'Aaaaa::Bbbb::Cccc') will be converted to the path 'Aaaaa/Bbbb/Cccc'(or 'Aaaaa\Bbbb\Cccc'
+    for Microsoft Windows) and the absolute path to the your Model dir will looks like '<your app>/lib/Aaaaa/Bbbb/Cccc/Model'
+    and all Model classes will be sought in this direcory.
 
 =back
 
@@ -243,93 +237,87 @@ and all Model classes will be sought in this direcory.
 
 =head1 EXAMPLE
 
-# the example of a new application:
-% cpan install Mojolicious::Plugin::BModel
-% mojo generate app MyApp
-% cd my_app/
-% vim lib/MyApp.pm
+    # the example of a new application:
+    % cpan install Mojolicious::Plugin::BModel
+    % mojo generate app MyApp
+    % cd my_app/
+    % vim lib/MyApp.pm
 
-# edit file:
-```perl
-package MyApp;
+    # edit file:
+    package MyApp;
 
-use Mojo::Base 'Mojolicious';
+    use Mojo::Base 'Mojolicious';
 
-sub startup {
-    my $self = shift;
+    sub startup {
+        my $self = shift;
 
-    $self->config->{testkey} = 'MyTestValue';
+        $self->config->{testkey} = 'MyTestValue';
 
-    $self->plugin( 'BModel' ); # used the default options
+        $self->plugin( 'BModel' ); # used the default options
 
-    my $r = $self->routes;
-    $r->get('/')->to( 'root#index' );
-}
+        my $r = $self->routes;
+        $r->get('/')->to( 'root#index' );
+    }
 
-1;
-```
+    1;
 
-# end of edit file
+    # end of edit file
 
-# create a new controller
+    # create a new controller
 
-% touch lib/Controller/Root.pm
-% vim lib/Controller/Root.pm
+    % touch lib/Controller/Root.pm
+    % vim lib/Controller/Root.pm
 
-# edit file
+    # edit file
 
-```perl
-package MyApp::Controller::Root;
+    package MyApp::Controller::Root;
 
-use Mojo::Base 'Mojolicious::Controller';
+    use Mojo::Base 'Mojolicious::Controller';
 
-sub index {
-    my $self = shift;
+    sub index {
+        my $self = shift;
 
-    my $testkey_val = $self->model('MyModel')->get_conf_key('testkey');
-    $self->render( text => 'Value: ' . $testkey_val );
-}
+        my $testkey_val = $self->model('MyModel')->get_conf_key('testkey');
+        $self->render( text => 'Value: ' . $testkey_val );
+    }
 
-1;
-```
+    1;
 
-# end of edit file
+    # end of edit file
 
-# When you connect, the plugin will check the folder "lib/MyApp/Model" exists.
-# By defaut the namespace 'MyApp' will be used.
-# If the folder 'lib/MyApp/Model' does not exist, this will be created.
-# The method 'app' of base model contains a link to your application.
-# The method 'config' of base model contains a link to config of yor application.
+    # When you connect, the plugin will check the folder "lib/MyApp/Model" exists.
+    # By defaut the namespace 'MyApp' will be used.
+    # If the folder 'lib/MyApp/Model' does not exist, this will be created.
+    # The method 'app' of base model contains a link to your application.
+    # The method 'config' of base model contains a link to config of yor application.
 
-# create a new model
-% touch lib/MyApp/Model/MyModel.pm
-% vim lib/MyApp/Model/MyModel.pm
+    # create a new model
+    % touch lib/MyApp/Model/MyModel.pm
+    % vim lib/MyApp/Model/MyModel.pm
 
-# edit file
+    # edit file
 
-```perl
-package MyApp::Model::MyModel;
+    package MyApp::Model::MyModel;
 
-use strict;
-use warnings;
+    use strict;
+    use warnings;
 
-use Mojo::Base 'Mojolicious::BModel::Base';
+    use Mojo::Base 'Mojolicious::BModel::Base';
 
-sub get_conf_key {
-    my ( $self, $key ) = @_;
+    sub get_conf_key {
+        my ( $self, $key ) = @_;
 
-    return $self->config->{ $key } || '';
-}
+        return $self->config->{ $key } || '';
+    }
 
-1;
-```
+    1;
 
-# end of edit file
+    # end of edit file
 
-% morbo -v script/my_app
+    % morbo -v script/my_app
 
-# Open in your browser address http://127.0.0.1:3000 and
-# you'll see text 'Value: MyTestValue'
+    # Open in your browser address http://127.0.0.1:3000 and
+    # you'll see text 'Value: MyTestValue'
 
 
 =head1 LICENSE
